9b2f89d4e4f4c9a24e743892c24c637fd3470565
Leandro Melo <leleoaqui@gmail.com>
2023-04-13 00:15:35 -0300

    Paginacao com parametros opcionas na requisicao

--- src/main/java/com/servegame/lmelo/cursomc/CursomcApplication.java
+++ src/main/java/com/servegame/lmelo/cursomc/CursomcApplication.java
@@ -70,6 +70,14 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		Categoria cat1 = new Categoria(null,"Informática");
 		Categoria cat2 = new Categoria(null,"Escritório");
+		Categoria cat3 = new Categoria(null,"Cama, Mesa e Banho");
+		Categoria cat4 = new Categoria(null,"Perfumaria");
+		Categoria cat5 = new Categoria(null,"Moda Feminina");
+		Categoria cat6 = new Categoria(null,"Moda Masculina");
+		Categoria cat7 = new Categoria(null,"Moda Infantil");
+		Categoria cat8 = new Categoria(null,"Esportes");
+		Categoria cat9 = new Categoria(null,"Brinquedos");
+		Categoria cat10 = new Categoria(null,"Eletrodomésticos");
 				
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
@@ -82,7 +90,7 @@ public class CursomcApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
-		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2,cat3,cat4,cat5,cat6,cat7,cat8,cat9,cat10));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 
 		Estado est1 = new Estado(null, "Paraíba");
