567a999b5333c37e3f5184385ffa6451b90684fb
Leandro Melo <leleoaqui@gmail.com>
2023-04-12 23:21:13 -0300

    Deletando uma Categoria com DELETE

--- src/main/java/com/servegame/lmelo/cursomc/services/CategoriaService.java
+++ src/main/java/com/servegame/lmelo/cursomc/services/CategoriaService.java
@@ -3,30 +3,43 @@ package com.servegame.lmelo.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.servegame.lmelo.cursomc.domain.Categoria;
 import com.servegame.lmelo.cursomc.repositories.CategoriaRepository;
+import com.servegame.lmelo.cursomc.services.exception.DataIntegrityException;
 import com.servegame.lmelo.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado Id: " + id + ", Tipo: " + Categoria.class.getName()));
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
-	
+
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos!!");
+		}
 	}
 }
